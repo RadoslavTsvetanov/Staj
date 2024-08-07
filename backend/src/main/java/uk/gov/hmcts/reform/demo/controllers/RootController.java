cf4c9a3dce007060fc5d181a443d982e4e5b4750
RadoslavTsvetanov <radoslavcvetanov5@gmail.com>
2024-08-07 14:17:18 +0300

    connected postgre db to backend

--- backend/src/main/java/uk/gov/hmcts/reform/demo/controllers/RootController.java
+++ backend/src/main/java/uk/gov/hmcts/reform/demo/controllers/RootController.java
@@ -1,8 +1,14 @@
 package uk.gov.hmcts.reform.demo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.sql.DataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
 
 import static org.springframework.http.ResponseEntity.ok;
 
---
+++ backend/src/main/java/uk/gov/hmcts/reform/demo/models/User.java
@@ -0,0 +1,5 @@
+package uk.gov.hmcts.reform.demo.models;
+public class User {
+
+}
+
