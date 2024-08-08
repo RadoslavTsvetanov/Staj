64006f428343ce1011afc507a838e489bbcc2ca6
yorizova <yoanaantonova9907@gmail.com>
2024-08-08 15:35:52 +0300

    waves

--- frontend/src/app/auth/signup/info/page.tsx
+++ frontend/src/app/auth/signup/info/page.tsx
@@ -33,15 +33,6 @@ export default function InfoRoute() {
                             id="dob"
                           />
                         </div>
-                        <label className="block text-sm font-medium text-gray-700">
-                            Budget
-                        </label>
-                        <div className="mt-1">
-                            <input
-                            className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                            required
-                            />
-                        </div>
                     </div>
                 </div>
             </div>
--- frontend/src/app/auth/signup/page.tsx
+++ frontend/src/app/auth/signup/page.tsx
@@ -7,8 +7,15 @@ import Earth from '../../../../public/Earth';
 
 export default function SignUpRoute() {
   return (
-    <div className="flex flex-col justify-center py-12 sm:px-6 lg:px-8">
-      <div className="mt-4 sm:mx-auto sm:w-full sm:max-w-md">
+    <div className="relative flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+      {/* Wave animation */}
+      <div className='box'>
+        <div className='wave -one'></div>
+        <div className='wave -two'></div>
+        <div className='wave -three'></div>
+      </div>
+      
+      <div className="mt-4 sm:mx-auto sm:w-full sm:max-w-md z-10">
         <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
           <form method="POST" action="#">
             <div>
@@ -117,6 +124,115 @@ export default function SignUpRoute() {
           </form>
         </div>
       </div>
+
+      <style jsx>{`
+        body {
+          margin: 0;
+          padding: 0;
+          background-color: #0e6cc4;
+          overflow-x: hidden;
+          overflow-y: hidden;
+        }
+
+        .box {
+          position: fixed;
+          top: 0;
+          transform: rotate(80deg);
+          left: 0;
+        }
+
+        .wave {
+          position: fixed;
+          top: 0;
+          left: 0;
+          opacity: .4;
+          position: absolute;
+          top: 3%;
+          left: 10%;
+          background: #0af;
+          width: 1500px;
+          height: 1300px;
+          margin-left: -150px;
+          margin-top: -250px;
+          transform-origin: 50% 48%;
+          border-radius: 43%;
+          animation: drift 7000ms infinite linear;
+        }
+
+        .wave.-three {
+          animation: drift 7500ms infinite linear;
+          position: fixed;
+          background-color: #77daff;
+        }
+
+        .wave.-two {
+          animation: drift 3000ms infinite linear;
+          opacity: .1;
+          background: black;
+          position: fixed;
+        }
+
+        .box:after {
+          content: '';
+          display: block;
+          left: 0;
+          top: 0;
+          width: 100%;
+          height: 100%;
+          z-index: 11;
+          transform: translate3d(0, 0, 0);
+        }
+
+        @keyframes drift {
+          from { transform: rotate(0deg); }
+          from { transform: rotate(360deg); }
+        }
+
+        .contain {
+          animation-delay: 4s;
+          z-index: 1000;
+          position: fixed;
+          top: 0;
+          left: 0;
+          bottom: 0;
+          right: 0;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          background: #25a7d7;
+          background: linear-gradient(#25a7d7, #25a7d7);
+        }
+
+        .icon {
+          width: 100px;
+          height: 100px;
+          margin: 0 5px;
+        }
+
+        .icon:nth-child(2) img { animation-delay: 0.2s; }
+        .icon:nth-child(3) img { animation-delay: 0.3s; }
+        .icon:nth-child(4) img { animation-delay: 0.4s; }
+
+        .icon img {
+          animation: anim 2s ease infinite;
+          transform: scale(0,0) rotateZ(180deg);
+        }
+
+        @keyframes anim {
+          0% {
+            transform: scale(0,0) rotateZ(-90deg); opacity:0;
+          }
+          30% {
+            transform: scale(1,1) rotateZ(0deg); opacity:1;
+          }
+          50% {
+            transform: scale(1,1) rotateZ(0deg); opacity:1;
+          }
+          80% {
+            transform: scale(0,0) rotateZ(90deg); opacity:0;
+          }
+        }
+      `}</style>
     </div>
   );
 }
