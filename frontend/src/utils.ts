8630581e7a39ca2bddbdb04e7e6450b7e4fcabeb
RadoslavTsvetanov <radoslavcvetanov5@gmail.com>
2024-08-09 18:57:20 +0300

    made some utils

---
+++ frontend/src/utils.ts
@@ -0,0 +1,26 @@
+class Cookie{ // Note: having name here and later in the cookie manager is a but redundant but it gets the job done 
+    private name: string
+    constructor(name: string) {
+        this.name = name
+    }
+
+    get() {
+        return document.cookie.split(';').find(c => c.trim().startsWith(`${this.name}=`))?.split('=')[1]
+    }
+
+    set(value: string, expires?: Date) {
+        document.cookie = `${this.name}=${value}${expires? `; expires=${expires.toUTCString()}` : ''}`
+    }
+
+}
+class CookieManager{
+    public authToken = new Cookie("authToken")
+}
+
+export const cookies = new CookieManager() 
+// --------------------------
+
+
+class Api{ // we will generate it from the open api docs from spring so dont do it manually
+
+}
