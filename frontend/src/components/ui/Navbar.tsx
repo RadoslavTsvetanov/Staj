2ddaffc26b27949ac9aa2a81b927ad637591d345
yorizova <yoanaantonova9907@gmail.com>
2024-08-13 11:52:28 +0300

    navbar

---
+++ frontend/src/components/ui/Navbar.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import Link from "next/link";
+
+export const Navbar = () => {
+    
+        return (
+        <nav className="bg-gray-800 text-white fixed top-0 left-0 w-full p-4 sm:p-4 md:flex md:justify-between md:items-center z-20">
+            <div className="container mx-auto flex justify-between items-center">
+                <a href="" className="text-2xl font-bold">
+                    Project
+                </a>
+                <div>
+                    <Link href="../../app/home" className="mx-2 hover:text-grey-300">
+                    Home
+                    </Link>
+                    <Link href="../../app/about" className="mx-2 hover:text-grey-300">
+                    About
+                    </Link>
+                    <Link href="/" className="mx-2 hover:text-grey-300">
+                    Contacts
+                    </Link>
+                </div>
+            </div>
+        </nav>
+    );
+};
