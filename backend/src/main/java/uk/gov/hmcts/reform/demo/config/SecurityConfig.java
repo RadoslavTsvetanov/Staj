6af77e22e5dd66808f44f426e7037d48d209f348
LiliShishkova <129530357+LiliShishkova@users.noreply.github.com>
2024-08-13 12:04:23 +0300

    SecurityConfig

---
+++ backend/src/main/java/uk/gov/hmcts/reform/demo/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package uk.gov.hmcts.reform.demo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import uk.gov.hmcts.reform.demo.filters.AuthTokenFilter;
+
+@Configuration
+public class SecurityConfig {
+
+    @Autowired
+    private AuthTokenFilter authTokenFilter;
+
+    @Bean
+    public FilterRegistrationBean<AuthTokenFilter> loggingFilter(){
+        FilterRegistrationBean<AuthTokenFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(authTokenFilter);
+        registrationBean.addUrlPatterns("/api/*");
+        return registrationBean;
+    }
+}
+
