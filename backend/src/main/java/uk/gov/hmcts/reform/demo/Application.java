c85dcfa13407d49d181f79d24544cd6546e797cb
LiliShishkova <129530357+LiliShishkova@users.noreply.github.com>
2024-08-16 09:35:59 +0300

    OpenAIServiceCustomInterest

--- backend/src/main/java/uk/gov/hmcts/reform/demo/Application.java
+++ backend/src/main/java/uk/gov/hmcts/reform/demo/Application.java
@@ -1,18 +1,33 @@
 package uk.gov.hmcts.reform.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
+import uk.gov.hmcts.reform.demo.services.OpenAIService;
 
 @SpringBootApplication(scanBasePackages = {"uk.gov.hmcts.reform.demo" })
-@SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
 @EnableJpaRepositories(basePackages = "uk.gov.hmcts.reform.demo.repositories")
 @EntityScan(basePackages = "uk.gov.hmcts.reform.demo")
-public class Application {
+//public class Application {
+
+ //   public static void main(final String[] args) {
+   //     SpringApplication.run(Application.class, args);
+   // }
+//}
+
+public class Application implements CommandLineRunner {
+    @Autowired
+    private OpenAIService openAIService;
 
     public static void main(final String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        openAIService.runTest();
     }
 }
