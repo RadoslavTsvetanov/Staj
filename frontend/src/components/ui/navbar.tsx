253a3dfe9f24fa4731e88fdf4de266f9626c54f3
Monika Georgieva <monikageorgieva950@gmail.com>
2024-08-16 15:50:41 +0300

    better navbar + menu button

--- frontend/src/components/ui/navbar.tsx
+++ frontend/src/components/ui/navbar.tsx
@@ -1,35 +1,25 @@
 "use client";
 
 import Link from "next/link";
+import Image from "next/image";
 import Logo from './Logo';
 
 const Navbar = () => {
     
         return (
-          <div style={{ height: '10vh' }} >
-        <nav className="bg-gray-800 text-white fixed top-0 left-0 w-full p-4 sm:p-4 md:flex md:justify-between md:items-center z-20">
-            <div className="container mx-auto flex justify-between items-center">
+          <nav className="h-[10vh] w-full flex bg-gray-800 text-white p-2 sm:p-2 md:flex md:justify-between md:items-center z-20" >
+
+            <div className=" container flex  items-center">
                <Logo/>
-                <a href="" className="text-2xl font-bold">
+                <h1 className="text-2xl font-bold ml-5">
                     Project
-                </a>
-                <div>
-                    <Link href="../../app/about" className="ml-8 hover:text-gray-300">
-                        About
-                    </Link>
-                    <Link href="#contacts" className="ml-8 hover:text-gray-300">
-                        Contacts
-                    </Link>
-                    {/* <input 
-                        type="button" 
-                        onClick={() => location.href='../../../auth/signup'} 
-                        value="Sign up" 
-                        className="ml-16 py-2 px-4 bg-[rgba(115,209,72,0.2)] border-transparent text-2sm font-medium rounded-md text-white hover:bg-[rgba(115,209,72,0.3)]" 
-                    /> */}
-                </div>
+                </h1>
             </div>
+            <div className="flex pr-3">
+                    <Image src="/images/menu.png" alt="Logo" width={32} height={32} />
+                </div>
+        
         </nav>
-        </div>
     );
 };
 
