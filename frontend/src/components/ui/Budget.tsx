026dae31ff7bcde4b45ebba631a4711997de66a3
Monika Georgieva <monikageorgieva950@gmail.com>
2024-08-17 17:41:02 +0300

    budget component

--- frontend/src/components/ui/Budget.tsx
+++ frontend/src/components/ui/Budget.tsx
@@ -6,6 +6,7 @@ const BudgetSelector = () => {
   const [isSelectedBudget, selectBudget] = useState(false);
 
   const handleClick = (index: number) => {
+    if (isSelectedBudget) return;
     setSelectedBudget(index);
     selectBudget(true);
     handleBudgetChange(index);
@@ -15,25 +16,30 @@ const BudgetSelector = () => {
     console.log('Selected Budget:', budget);
   };
 
+  const handleEditClick = () => {
+    selectBudget(false); 
+    setSelectedBudget(4);
+  };
+
   return (
-    <div className="p-4 bg-blue-100 ">
+    <div className="pr-4 bg-blue-100 ">
       <form>
     <div className="flex items-center space-x-2">
       <span className="text-lg">Budget</span>
       <div className="flex">
-        {[1, 2, 3, 4].map((level) => (
-          <span
-            key={level}
-            className={`cursor-pointer text-2xl ${
-              selectedBudget >= level ? 'text-green-700' : 'text-gray-800'
-            }`}
-            onClick={() => handleClick(level)}
-          >
-            $
-          </span>
-        ))}
-      </div>
-      <Image src="/images/edit.png" alt="editB" onClick={()=>selectBudget(false)} width={20} height={20} />
+            {[1, 2, 3, 4].map((level) => (
+              <span
+                key={level}
+                className={`cursor-pointer text-2xl ${
+                  selectedBudget >= level ? 'text-green-700' : 'text-gray-800'
+                } ${isSelectedBudget && 'cursor-not-allowed'}`} 
+                onClick={() => handleClick(level)}
+              >
+                $
+              </span>
+            ))}
+          </div>
+      <Image src="/images/edit.png" alt="editB" onClick={handleEditClick} width={20} height={20} />
     </div>
     </form>
     </div>
