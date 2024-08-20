4e312b3699bbafe9996b79523632d19c6fd38bc9
Monika Georgieva <monikageorgieva950@gmail.com>
2024-08-20 13:32:10 +0300

    main update, added features (#9)
    * BUTTON IN THE RIGHT POSITION
    
    * final commit for the day
    
    * polygon
    
    * slider
    
    * better navbar + menu button
    
    * dropdown menu started
    
    * menu
    
    * budget component
    
    * choosing dates , ugly start of the history  page
    
    * just a commit
    
    * address field working
    
    * that is for today
    
    * оправяне на забележки
    
    budget fix, pin onClick, pin move
    
    * uncomment

--- frontend/src/app/planning/page.tsx
+++ frontend/src/app/planning/page.tsx
@@ -1,35 +1,22 @@
 "use client";
 
-import { useState } from 'react';
-import Map from "@/components/ui/map";
+import { useState } from "react";
 import CurrentTripComp from "@/components/ui/currentTrip";
-import MarkerPin from "@/components/ui/marker";
-import Region from "@/components/ui/Region";
 import BackButton from "@/components/ui/BackButton";
-import Slider from "@/components/ui/Slider";
-import { useSearchParams } from 'next/navigation'; 
+import PlanningPageMap from "@/components/ui/PlanningPageMap";
 
 export default function PlanningPage() {
-    const searchParams = useSearchParams();
-    const [radius, setRadius] = useState(0);  
+  const [address, setAddress] = useState<string>("");
 
-    let lat = parseFloat(searchParams.get('lat') ?? '0');
-    let lng = parseFloat(searchParams.get('lng') ?? '0');
-    
-    const positionMarker = { lat, lng };
-    const center = { lat, lng };
+  const handleAddressChange = (newAddress: string) => {
+    setAddress(newAddress);
+  };
 
-    return (
-        <div className="relative h-[100vh] w-full">
-            <Map className="h-full w-full" center={center}> 
-                <Region center={positionMarker} radius={radius}/> 
-                <MarkerPin positionMarker={positionMarker} />
-            </Map>
-            <BackButton />
-            <CurrentTripComp />
-            <div className="absolute bottom-10 left-1/4 transform -translate-x-1/2">
-                <Slider radius={radius} setRadius={setRadius} />
-            </div>
-        </div>
-    );
+  return (
+    <div className="relative h-[100vh] w-full">
+      <PlanningPageMap onAddressChange={handleAddressChange} />
+      <BackButton />
+      <CurrentTripComp location={address} />
+    </div>
+  );
 }
--- frontend/src/components/ui/map.tsx
+++ frontend/src/components/ui/map.tsx
@@ -1,5 +1,7 @@
 "use client";
+
 import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+import { useState, useEffect } from 'react';
 
 const containerStyle = {
   width: "100%",
@@ -7,7 +9,7 @@ const containerStyle = {
 };
 
 const mapOptions = {
-  zoom: 17,
+  zoom: 15,
   mapId: "MY_NEXTJS_MAP_ID",
   mapTypeControl: false,
   zoomControl: false,
@@ -19,31 +21,53 @@ const mapOptions = {
 interface MapProps {
   children?: React.ReactNode;
   className?: string;
-  center:  {
+  center: {
     lat: number,
     lng: number,
   };
+  onMapLoad?: (loaded: boolean) => void; 
+  onClick?: (event: any) => void;
 }
 
-const Map: React.FC<MapProps> = ({ children ,center}) => {
+const Map: React.FC<MapProps> = ({ children, center, onMapLoad,onClick }) => {
+  const [isMapLoaded, setIsMapLoaded] = useState<boolean>(false);
+
+
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
     googleMapsApiKey: process.env.NEXT_PUBLIC_MAPS_API_KEY as string,
     version: "weekly",
   });
 
-  return isLoaded ? (
-    <GoogleMap
-      mapContainerStyle={containerStyle}
-      options={mapOptions}
-      center={center}
-      zoom={10}
-    >
-    {children}
+  useEffect(() => {
+    if (isLoaded) {
+      setIsMapLoaded(true);
+      if (onMapLoad) {
+        onMapLoad(true); 
+      }
+    } else {
+      setIsMapLoaded(false);
+      if (onMapLoad) {
+        onMapLoad(false); 
+      }
+    }
+  }, [isLoaded, onMapLoad]);
 
-    </GoogleMap>
-  ) : (
-    <></>
+  return (
+    <>
+      {isMapLoaded ? (
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          options={mapOptions}
+          center={center}
+          onClick={onClick}
+        >
+          {children}
+        </GoogleMap>
+      ) : (
+        <div>Loading map...</div>
+      )}
+    </>
   );
 }
 
