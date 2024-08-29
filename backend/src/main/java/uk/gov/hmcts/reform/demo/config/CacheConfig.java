948a453b944097fe5f515fc347b1441435da4c69
LiliShishkova <129530357+LiliShishkova@users.noreply.github.com>
2024-08-29 11:08:17 +0300

    RedisCache

--- backend/src/main/java/uk/gov/hmcts/reform/demo/config/CacheConfig.java
+++ backend/src/main/java/uk/gov/hmcts/reform/demo/config/CacheConfig.java
@@ -3,13 +3,14 @@ package uk.gov.hmcts.reform.demo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import uk.gov.hmcts.reform.demo.services.Cache;
-import uk.gov.hmcts.reform.demo.services.InMemoryCache;
+import uk.gov.hmcts.reform.demo.services.RedisCache;
 
 @Configuration
 public class CacheConfig {
 
     @Bean
-    public Cache cache() {
-        return new InMemoryCache();
+    public Cache cache(RedisCache redisCache) {
+        return redisCache;
     }
 }
+
---
+++ backend/src/main/java/uk/gov/hmcts/reform/demo/config/RedisConfig.java
@@ -0,0 +1,23 @@
+package uk.gov.hmcts.reform.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.util.List;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, List<String>> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, List<String>> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericToStringSerializer<>(List.class));
+        return template;
+    }
+}
