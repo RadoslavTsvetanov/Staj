2e7a5bc76dce82ff81c11e814a1a30002dfc3f17
yorizova <yoanaantonova9907@gmail.com>
2024-09-02 15:38:52 +0300

    pics

--- frontend/src/app/error.tsx
+++ frontend/src/app/error.tsx
@@ -16,13 +16,8 @@ export default function Error({
  
   return (
     <div>
-      <h2>Something went wrong!</h2>
-      <button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
+      <h2>Something ffffffwent wrong!</h2>
+      <button onClick={() => reset()}>
         Try again
       </button>
     </div>
