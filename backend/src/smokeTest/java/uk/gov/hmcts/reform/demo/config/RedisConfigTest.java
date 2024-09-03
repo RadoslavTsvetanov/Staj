1275af9a77bccf82cbddeed45f99655b36a9956c
LiliShishkova <129530357+LiliShishkova@users.noreply.github.com>
2024-09-03 12:56:34 +0300

    RedisConfigTest

---
+++ backend/src/smokeTest/java/uk/gov/hmcts/reform/demo/config/RedisConfigTest.java
@@ -0,0 +1,31 @@
+package uk.gov.hmcts.reform.demo.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+class RedisConfigTest {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Test
+    void testJedisConnectionFactoryBean() {
+        JedisConnectionFactory jedisConnectionFactory = applicationContext.getBean(JedisConnectionFactory.class);
+        assertNotNull(jedisConnectionFactory);
+    }
+
+    @Test
+    void testRedisTemplateBean() {
+        RedisTemplate<String, List<String>> redisTemplate = applicationContext.getBean("redisTemplate", RedisTemplate.class);
+        assertNotNull(redisTemplate);
+    }
+}
