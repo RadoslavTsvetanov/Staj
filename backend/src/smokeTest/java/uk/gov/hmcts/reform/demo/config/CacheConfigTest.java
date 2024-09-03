8445e3e680c73f14b0e0eac7e0e9421199982955
LiliShishkova <129530357+LiliShishkova@users.noreply.github.com>
2024-09-03 12:58:43 +0300

    CacheConfigTest

---
+++ backend/src/smokeTest/java/uk/gov/hmcts/reform/demo/config/CacheConfigTest.java
@@ -0,0 +1,22 @@
+package uk.gov.hmcts.reform.demo.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import uk.gov.hmcts.reform.demo.services.Cache;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+class CacheConfigTest {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Test
+    void testCacheBean() {
+        Cache cache = applicationContext.getBean(Cache.class);
+        assertNotNull(cache);
+    }
+}
